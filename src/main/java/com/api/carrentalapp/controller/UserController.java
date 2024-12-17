e64d313d1de5aedbaa4fe0843f6a98cae28cc6da
gcancomert@hotmail.com <138134393+Galanovic@users.noreply.github.com>
2024-12-17 21:40:34 +0300

    Customer Operations

--- src/main/java/com/api/carrentalapp/controller/UserController.java
+++ src/main/java/com/api/carrentalapp/controller/UserController.java
@@ -46,48 +46,5 @@ public class UserController {
     }
 
 }
-/*
-private UserRepository userRepository;
 
-public UserController(UserRepository userRepository) {
-    this.userRepository = userRepository;
-}
-
-//yukarıdaki users path'i için çalışıcak
-@GetMapping
-public List<User> getAllUsers(){
-    return userRepository.findAll();
-}
-
-@PostMapping
-public User createUser(@RequestBody User newUser){
-    return userRepository.save(newUser);
-}
-
-@GetMapping("/{userId}")    //youtube videosunda ki isimlendirme getOneUser
-public User getByUserId(@PathVariable Long userId){
-    //custom exception yapılıcak!!
-    return userRepository.findById(userId).orElse(null);
-}
-
-@PutMapping("/{userId}")
-public User updateByUser(@PathVariable Long userId, @RequestBody User newUser){
-    Optional<User> user = userRepository.findById(userId);
-    if(user.isPresent()) {
-        User foundUser = user.get();
-        foundUser.setUsername(newUser.getUsername());
-        foundUser.setPassword(newUser.getPassword());
-        userRepository.save(foundUser);
-        return foundUser;
-    } else {
-        return null;
-    }
-}
-
-@DeleteMapping("/{userId}")
-public void deleteUser(@PathVariable Long userId){
-    userRepository.deleteById(userId);
-}
-
- */
 
