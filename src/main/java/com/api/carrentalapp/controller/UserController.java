9f1b160202a7b07022ffcd15cf11909c25c08ae5
gcancomert@hotmail.com <138134393+Galanovic@users.noreply.github.com>
2024-12-17 21:44:12 +0300

    Customer Operations

--- src/main/java/com/api/carrentalapp/controller/UserController.java
+++ src/main/java/com/api/carrentalapp/controller/UserController.java
@@ -13,7 +13,7 @@ public class UserController {
 
     private UserService userService;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
